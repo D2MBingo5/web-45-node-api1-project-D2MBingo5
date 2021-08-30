5ad3c64fe2644262dd3c6d31b7577e495e159c1c
Leu Family <romanianfobjr@gmail.com>
2021-08-30 16:59:12 -0500

    hello world endpoint created

--- api/server.js
+++ api/server.js
@@ -5,4 +5,8 @@ const server = express()
 
 server.use(express.json())
 
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'hello world'})
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
