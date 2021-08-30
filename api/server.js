bc6d6686b111a3fc2c682606662053237cc05dcd
Leu Family <romanianfobjr@gmail.com>
2021-08-30 17:11:58 -0500

    server.get commands added

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
+const User = require('./users/model')
 
 const server = express()
 
@@ -7,6 +8,34 @@ server.use(express.json())
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'hello world'})
+})
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            if(users) {
+                res.json(users)
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "The users information could not be retrieved"})
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if(user) {
+                res.json(user)
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "The user information could not be retrieved" })
+        })
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
