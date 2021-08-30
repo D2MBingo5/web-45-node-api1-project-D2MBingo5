7c0c7528ed1ba90549ffd21559c1bd2dc72f0699
Leu Family <romanianfobjr@gmail.com>
2021-08-30 18:11:06 -0500

    server.put command created

--- api/server.js
+++ api/server.js
@@ -56,12 +56,18 @@ server.get('/api/users/:id', (req, res) => {
 })
 
 // server.delete('api/users/:id', async (req, res) => {
-//     const possibleUser = await User.findById(req.params.id)
+//     try {
+//         const possibleUser = await User.findById(req.params.id)
 //     if (!possibleUser) {
 //         res.status(404).json({ message: "The user with the specified ID does not exist" })
 //     } else {
 //         const deletedUser = await User.remove(possibleUser.id)
 //         res.json(deletedUser)
+//     }
+//     } catch (err) {
+//         res.status(500).json({
+//             message: "The user could not be removed"
+//         })
 //     }
 // })
 
@@ -82,5 +88,26 @@ server.delete('/api/users/:id', (req, res) => {
 })
 
 // server.put('/api/users/:id', async )
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({ message: "Please provide name and bio for the user" })
+            } else {
+                const userUpdate = await User.update(req.params.id, req.body)
+                res.status(200).json(userUpdate)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The user information could not be modified"
+        })
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
